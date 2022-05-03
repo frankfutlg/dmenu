8d48ef0398c61ab1f0d234775cfbca62215ee5a3
frankfutlg <frankiekittie43@outlook.com>
2022-05-03 18:20:52 -0300

    first commit

---
+++ config.def.h
@@ -0,0 +1,17 @@
+static int topbar = 1;               
+static const char *fonts[]          = { "JetBrains Mono Medium:style=Medium,Regular:size=10:antialias=true:autohint=true",
+					"Symbols Nerd Font Mono:style=2048-em:pixelsize=14:antialias=true:autohint=true",
+					"Material Design Icons:style=Regular:pixelsize=15:antialias=true:autohint=true",
+					"Noto Color Emoji:style=Regular:size=10:antialias=true:autohint=true", 
+					"IPAゴシック:style=Regular:size=12:antialias=true:autohint=true" };
+
+static const char *prompt      = NULL;
+
+/* colorscheme */
+#include "themes/catppuccin.h"
+
+static unsigned int lines      = 0;
+static unsigned int lineheight = 28;
+static unsigned int min_lineheight = 8;
+
+static const char worddelimiters[] = " ";
---
+++ themes/catppuccin.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	//     		fg          bg       
+	[SchemeNorm] = { "#C3BAC6", "#161320" },
+	[SchemeSel] = { "#96CDFB", "#161320" },
+	[SchemeOut] = { "#DDB6F2", "#F28FAD" },
+};
